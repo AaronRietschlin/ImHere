dd45e83eb180b3184559b3c09a3f8f8923a52a0e
Aaron Rietschlin <nemisis.82@gmail.com>
2014-07-13 15:39:51 -0400

    Added example JSON to the assets. added method to get it from there.

--- ImHere/src/main/java/com/asa/imhere/service/CheckinService.java
+++ ImHere/src/main/java/com/asa/imhere/service/CheckinService.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.text.TextUtils;
 
+import com.asa.imhere.IHApplication;
 import com.asa.imhere.lib.foursquare.FsUtils;
 import com.asa.imhere.lib.foursquare.FsVenue;
 import com.asa.imhere.notifications.CheckinNotification;
@@ -12,6 +13,8 @@ import com.crashlytics.android.Crashlytics;
 import com.koushikdutta.ion.Ion;
 import com.koushikdutta.ion.Response;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.concurrent.ExecutionException;
 
 import timber.log.Timber;
@@ -66,19 +69,25 @@ public class CheckinService extends IntentService {
         CheckinNotification.cancel(getApplicationContext());
         String url = FsUtils.constructCheckinsUrl(getApplicationContext(), venueId, -1L, -1L, -1, null, null);
         try {
-            Response<String> response = Ion.with(getApplicationContext()).load("POST", url)
-                    .group(TAG).asString().withResponse().get();
-            if (response == null || response.getHeaders() == null) {
-                logToCrashlytics(url, null, -1);
-                Timber.e(new Exception("Checkin response was null"), "");
-                return;
-            }
-            int responseCode = response.getHeaders().getResponseCode();
-            String checkinJsonResult = response.getResult();
-            if(TextUtils.isEmpty(checkinJsonResult)){
-                logToCrashlytics(url, checkinJsonResult, responseCode);
-                Timber.e(new Exception("Checkin response was null"), "");
-                return;
+            String checkinJsonResult = "";
+            if (!IHApplication.DEBUG) {
+                Response<String> response = Ion.with(getApplicationContext()).load("POST", url)
+                        .group(TAG).asString().withResponse().get();
+                if (response == null || response.getHeaders() == null) {
+                    logToCrashlytics(url, null, -1);
+                    Timber.e(new Exception("Checkin response was null"), "");
+                    return;
+                }
+                int responseCode = response.getHeaders().getResponseCode();
+                checkinJsonResult = response.getResult();
+                if (TextUtils.isEmpty(checkinJsonResult)) {
+                    logToCrashlytics(url, checkinJsonResult, responseCode);
+                    Timber.e(new Exception("Checkin response was null"), "");
+                    return;
+                }
+            } else {
+                checkinJsonResult = debugJson();
+                Timber.e("");
             }
             // TODO -
         } catch (InterruptedException e) {
@@ -96,15 +105,34 @@ public class CheckinService extends IntentService {
         // TODO - if getting the location, close it here.
     }
 
-    private void logToCrashlytics(String url, String jsonResult, int responseCode){
-        if(!TextUtils.isEmpty(url)){
+    private void logToCrashlytics(String url, String jsonResult, int responseCode) {
+        if (!TextUtils.isEmpty(url)) {
             Crashlytics.setString("URL", url);
         }
-        if(!TextUtils.isEmpty(jsonResult)){
+        if (!TextUtils.isEmpty(jsonResult)) {
             Crashlytics.setString("RESULT", jsonResult);
         }
-        if(responseCode > 0){
+        if (responseCode > 0) {
             Crashlytics.setInt("RSPONSE_CODE", responseCode);
         }
+    }
+
+    /**
+     * Returns an example version of the response from the /assets folder. This way, we don't have to
+     * actually checkin.
+     */
+    private String debugJson() {
+        String json = null;
+        try {
+            InputStream is = getAssets().open("example_checkin_response.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            Timber.e(ex, "");
+        }
+        return json;
     }
 }
